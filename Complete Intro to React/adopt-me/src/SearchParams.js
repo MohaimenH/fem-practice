3d836e5a69d92ce24aea034815201a8dd165bf8c
MohaimenH <mohaimenhassan08@gmail.com>
2021-07-29 10:27:56 +0600

    Hooks

---
+++ Complete Intro to React/adopt-me/src/SearchParams.js
@@ -0,0 +1,15 @@
+const SearchParams = () => {
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input id="location" value="Seattle, WA" placeholder="Location" />
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
