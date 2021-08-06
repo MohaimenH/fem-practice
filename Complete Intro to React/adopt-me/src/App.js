dec8861e2d1d22f1b2976fd8366cadfab625354d
MohaimenH <mohaimenhassan08@gmail.com>
2021-08-06 18:10:32 +0600

    Class components

--- Complete Intro to React/adopt-me/src/App.js
+++ Complete Intro to React/adopt-me/src/App.js
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import SearchParams from "./SearchParams";
 import Details from "./Details";
@@ -10,8 +11,12 @@ import Details from "./Details";
 const App = () => {
   return (
     <div>
-      <h1>Adopt Me!</h1>
       <Router>
+        <header>
+          <Link to="/">
+            <h1>Adopt Me!</h1>
+          </Link>
+        </header>
         <Switch>
           <Route path="/details/:id">
             <Details />
--- Complete Intro to React/adopt-me/src/Details.js
+++ Complete Intro to React/adopt-me/src/Details.js
@@ -1,7 +1,36 @@
 import React from "react";
+import { Component } from "react";
+import { withRouter } from "react-router-dom";
 
-const Details = () => {
-  return <h2>Hi, this is details!</h2>;
-};
+class Details extends Component {
+  constructor() {
+    super();
 
-export default Details;
+    this.state = { loading: true };
+  }
+
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
+    );
+
+    const json = await res.json();
+    this.setState(Object.assign({ loading: false }, json.pets[0]));
+  }
+
+  render() {
+    const { animal, breed, city, state, description, name } = this.state;
+    return (
+      <div className="details">
+        <h1>{name}</h1>
+        <h2>
+          {animal} - {breed} - {city} - {state}
+        </h2>
+        <button>Adopt {name}</button>
+        <p>{description}</p>
+      </div>
+    );
+  }
+}
+
+export default withRouter(Details);
