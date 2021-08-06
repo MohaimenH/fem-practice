4add3467573246a6de5e50435964fcbf0260b7a7
MohaimenH <mohaimenhassan08@gmail.com>
2021-08-06 19:56:29 +0600

    Added Carousel

---
+++ Complete Intro to React/adopt-me/src/Carousel.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+class Carousel extends Component {
+  constructor() {
+    super();
+
+    this.state = { active: 0 };
+  }
+  render() {
+    const { active } = this.state;
+    const { images } = this.props;
+
+    return (
+      <div className="carousel">
+        <img src={images[active]} alt="animal" />
+        <div className="carousel-smaller">
+          {images.map((photo, index) => (
+            <img
+              key={photo}
+              src={photo}
+              className={index === active ? "active" : ""}
+              alt="animal-bottom"
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+Carousel.defaultProps = {
+  images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
+};
+
+export default Carousel;
--- Complete Intro to React/adopt-me/src/Details.js
+++ Complete Intro to React/adopt-me/src/Details.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Component } from "react";
 import { withRouter } from "react-router-dom";
 
+import Carousel from "./Carousel";
+
 class Details extends Component {
   constructor() {
     super();
@@ -19,9 +21,15 @@ class Details extends Component {
   }
 
   render() {
-    const { animal, breed, city, state, description, name } = this.state;
+    if (this.state.loading) {
+      return <h2>Loading</h2>;
+    }
+
+    const { animal, breed, city, state, description, name, images } =
+      this.state;
     return (
       <div className="details">
+        <Carousel images={images} />
         <h1>{name}</h1>
         <h2>
           {animal} - {breed} - {city} - {state}
