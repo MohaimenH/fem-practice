51e4268c95b8d8e95bb295e178de53251a4c5fbf
MohaimenH <mohaimenhassan08@gmail.com>
2021-08-23 19:35:05 +0600

    Finished Reddit CLI

--- Introduction to Node.js/reddit-cli/reddit.mjs
+++ Introduction to Node.js/reddit-cli/reddit.mjs
@@ -2,6 +2,28 @@
 
 console.log("Welcome to the Reddit CLI!");
 
-import fetch from "node.fetch";
+import fetch from "node-fetch";
 import open from "open";
 import yargs from "yargs";
+
+const { argv } = yargs(process.argv);
+// console.log(argv);
+
+const res = await fetch(`https://reddit.com/.json`);
+const data = await res.json();
+
+// console.log(data);
+
+const randomPost = data.data.children[Math.floor(Math.random() * 25)];
+
+// console.log(randomPost);
+
+if (argv.print) {
+    console.log(
+        `title: ${randomPost.data.title}\ndescription: ${randomPost.data.selftext}`
+    );
+}
+
+if (argv.open) {
+    open(randomPost.data.url);
+}
